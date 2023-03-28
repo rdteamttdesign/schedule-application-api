f3e84507daf2905cbacd3f80789d8f576ba7e5f7
tri <n.v.tri@ttdesignco.com>
2023-03-28 14:28:48 +0700

    Add project detail sample to API create project.

---
+++ Resources/projectdetail/PredecessorResource.cs
@@ -0,0 +1,8 @@
+﻿namespace SchedulingTool.Api.Resources.projectdetail;
+
+public class PredecessorResource
+{
+  public string Type { get; set; } = null!;
+  public string Id { get; set; } = null!;
+  public float LagDays { get; set; }
+}
---
+++ Resources/projectdetail/StepworkResource.cs
@@ -0,0 +1,16 @@
+﻿namespace SchedulingTool.Api.Resources.projectdetail;
+
+public class StepworkResource
+{
+  public int Start { get; set; }
+  public float Duration { get; set; }
+  public float PercentStepWork { get; set; }
+  public string Name { get; set; } = null!;
+  public string ParentTaskId { get; set; } = null!;
+  public string Id { get; set; } = null!;
+  public string Type { get; set; } = null!;
+  public string GroupId { get; set; } = null!;
+  public int DisplayOrder { get; set; }
+  public ICollection<PredecessorResource> Predecessors { get; set; } = null!;
+  public long ColorId { get; set; }
+}
