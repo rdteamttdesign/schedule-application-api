c93484220ac043de003b340fd9a61efa9749e295
tri <n.v.tri@ttdesignco.com>
2023-07-26 15:51:12 +0700

    Fix API project name list.

--- Persistence/Repositories/ProjectVersionRepository.cs
+++ Persistence/Repositories/ProjectVersionRepository.cs
@@ -11,6 +11,11 @@ public class ProjectVersionRepository : GenericRepository<ProjectVersion>, IProj
   {
   }
 
+  public async Task<IEnumerable<ProjectVersion>> GetAll()
+  {
+    return await _context.ProjectVersions.ToListAsync();
+  }
+
   public async Task<IEnumerable<ProjectVersion>> GetByProjectId( long projectId )
   {
     return await _context.ProjectVersions.Where( x => x.ProjectId == projectId ).ToListAsync();
